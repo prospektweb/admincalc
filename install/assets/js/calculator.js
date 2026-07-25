61dd8323a5bb683ec48993ff77031da0eb818974
PROSPEKT <prospektweb@gmail.com>
2026-07-25 10:34:00 +0500

    feat: publish entity property selector

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=8edab12',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=27424e4',
     apiBase: '/bitrix/tools/prospektweb.calc/',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
