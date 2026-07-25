994954b95a501bba6f64d9b4056956bbe7f57735
PROSPEKT <prospektweb@gmail.com>
2026-07-25 17:07:21 +0500

    Ship calculator catalog and slider fixes

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=4af45c3',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=07c8f24',
     apiBase: '/bitrix/tools/prospektweb.calc/',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
