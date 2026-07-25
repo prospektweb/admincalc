fbccbbb3b3623718f045319ed8fb8dcd85d9a5f8
PROSPEKT <prospektweb@gmail.com>
2026-07-25 20:18:00 +0500

    fix: refresh calcconfig asset cache key

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=07c8f24',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=0bbe805',
     apiBase: '/bitrix/tools/prospektweb.calc/',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
