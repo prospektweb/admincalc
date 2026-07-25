027666dd2d780a09acc83f1f467cd7595ab75e11
PROSPEKT <prospektweb@gmail.com>
2026-07-25 10:53:52 +0500

    build: publish themed logic editor controls

--- admin/calculator.php
+++ admin/calculator.php
@@ -89,7 +89,7 @@ require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_admin_a
 <div id="calc-container">
     <iframe 
         id="calc-iframe" 
-        src="/local/apps/prospektweb.calc/index.html?v=0dd5c4d505e5"
+        src="/local/apps/prospektweb.calc/index.html?v=a76390d15ec8"
         title="<?= Loc::getMessage('PROSPEKTWEB_CALC_IFRAME_TITLE') ?>">
     </iframe>
 </div>
--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=fba32f2',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=a76390d15ec8',
     apiBase: '/bitrix/tools/prospektweb.calc/',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
