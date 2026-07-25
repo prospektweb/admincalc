13584db92663ef34302fa96ddad36882871324c9
PROSPEKT <prospektweb@gmail.com>
2026-07-25 10:55:26 +0500

    fix(calc): publish selector toggle behavior

--- admin/calculator.php
+++ admin/calculator.php
@@ -89,7 +89,7 @@ require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_admin_a
 <div id="calc-container">
     <iframe 
         id="calc-iframe" 
-        src="/local/apps/prospektweb.calc/index.html?v=a76390d15ec8"
+        src="/local/apps/prospektweb.calc/index.html?v=30b39d228014"
         title="<?= Loc::getMessage('PROSPEKTWEB_CALC_IFRAME_TITLE') ?>">
     </iframe>
 </div>
--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=a76390d15ec8',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=30b39d228014',
     apiBase: '/bitrix/tools/prospektweb.calc/',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
