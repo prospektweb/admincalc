6cafec1ce929e64e295057f22b2d6c1b721b3509
PROSPEKT <prospektweb@gmail.com>
2026-07-25 08:46:07 +0500

    fix: route AI context edits to internal sliders

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=49f53ae',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=b1c7421',
     apiBase: '/bitrix/tools/prospektweb.calc/',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
