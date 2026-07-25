e2083e35c2fd551be467298c3decf58f6530b8c8
PROSPEKT <prospektweb@gmail.com>
2026-07-25 10:40:12 +0500

    feat: publish AI source value overrides

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=27424e4',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=0074c27',
     apiBase: '/bitrix/tools/prospektweb.calc/',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
