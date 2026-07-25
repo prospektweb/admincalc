63325b140cb8cf369a4d06528b9c7d1757fadbae
PROSPEKT <prospektweb@gmail.com>
2026-07-25 11:39:59 +0500

    fix(calc): publish AI source visibility controls

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=add4ef1feffe',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=3f07bd706a22',
     apiBase: '/bitrix/tools/prospektweb.calc/',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
