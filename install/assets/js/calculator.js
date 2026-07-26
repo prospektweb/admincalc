e50f1e98e2906a199b2e99d701e1f17719621a5a
PROSPEKT <prospektweb@gmail.com>
2026-07-26 16:23:56 +0500

    Refresh calculator document cache

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=132b1e0',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=132b1e035ef5',
     apiBase: '/bitrix/tools/prospektweb.calc/',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
