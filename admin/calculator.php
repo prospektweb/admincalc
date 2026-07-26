3b05157728885f8cb9c55c05c4eeae22f0791453
PROSPEKT <prospektweb@gmail.com>
2026-07-26 16:55:32 +0500

    Deploy entity icon state fix

--- admin/calculator.php
+++ admin/calculator.php
@@ -89,7 +89,7 @@ require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_admin_a
 <div id="calc-container">
     <iframe 
         id="calc-iframe" 
-        src="/local/apps/prospektweb.calc/index.html?v=75f240e1435e"
+        src="/local/apps/prospektweb.calc/index.html?v=bc6d5d021dc8"
         title="<?= Loc::getMessage('PROSPEKTWEB_CALC_IFRAME_TITLE') ?>">
     </iframe>
 </div>
