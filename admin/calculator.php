ca120ceb3792925506b25f8aaea1f0d0892e135f
PROSPEKT <prospektweb@gmail.com>
2026-07-30 10:25:41 +0500

    Publish condition branch slider fixes

--- admin/calculator.php
+++ admin/calculator.php
@@ -89,7 +89,7 @@ require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_admin_a
 <div id="calc-container">
     <iframe 
         id="calc-iframe" 
-        src="/local/apps/prospektweb.calc/index.html?v=de802c0eda1f"
+        src="/local/apps/prospektweb.calc/index.html?v=62b04fb55940"
         title="<?= Loc::getMessage('PROSPEKTWEB_CALC_IFRAME_TITLE') ?>">
     </iframe>
 </div>
--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=de802c0eda1f',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=62b04fb55940',
     apiBase: '/bitrix/tools/prospektweb.calc/',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
