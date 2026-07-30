0cbd61096a6e696db1012039d7094b73d1f60c96
PROSPEKT <prospektweb@gmail.com>
2026-07-30 16:24:18 +0500

    Publish conditional stage markers

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=34b14aa518a8',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=a99a1856cd09',
     apiBase: '/bitrix/tools/prospektweb.calc/',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
