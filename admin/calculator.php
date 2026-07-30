d36d10f02ae2acacae3f1edb8a82a1706e032902
PROSPEKT <prospektweb@gmail.com>
2026-07-30 11:24:25 +0500

    Publish nested subgroup hierarchy and sorting

--- admin/calculator.php
+++ admin/calculator.php
@@ -89,7 +89,7 @@ require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_admin_a
 <div id="calc-container">
     <iframe 
         id="calc-iframe" 
-        src="/local/apps/prospektweb.calc/index.html?v=2bed307a7b10"
+        src="/local/apps/prospektweb.calc/index.html?v=d523da3afd4b"
         title="<?= Loc::getMessage('PROSPEKTWEB_CALC_IFRAME_TITLE') ?>">
     </iframe>
 </div>
--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=2bed307a7b10',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=d523da3afd4b',
     apiBase: '/bitrix/tools/prospektweb.calc/',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
