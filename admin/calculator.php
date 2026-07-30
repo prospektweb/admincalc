b71da73bb41ef003e7da7291dde07f86ceafb261
PROSPEKT <prospektweb@gmail.com>
2026-07-30 12:49:04 +0500

    Align nested drag landing slot

--- admin/calculator.php
+++ admin/calculator.php
@@ -89,7 +89,7 @@ require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_admin_a
 <div id="calc-container">
     <iframe 
         id="calc-iframe" 
-        src="/local/apps/prospektweb.calc/index.html?v=d7c72c9ef4c2"
+        src="/local/apps/prospektweb.calc/index.html?v=e3de79b6418d"
         title="<?= Loc::getMessage('PROSPEKTWEB_CALC_IFRAME_TITLE') ?>">
     </iframe>
 </div>
--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=d7c72c9ef4c2',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=e3de79b6418d',
     apiBase: '/bitrix/tools/prospektweb.calc/',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
