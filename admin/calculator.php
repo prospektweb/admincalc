b0c3fbb9b1fa6e1074bda3686bbb9ba386c447e7
PROSPEKT <prospektweb@gmail.com>
2026-07-30 21:33:36 +0500

    Deploy refined calculation report UI

--- admin/calculator.php
+++ admin/calculator.php
@@ -89,7 +89,7 @@ require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_admin_a
 <div id="calc-container">
     <iframe 
         id="calc-iframe" 
-        src="/local/apps/prospektweb.calc/index.html?v=ff82d0bc7c44"
+        src="/local/apps/prospektweb.calc/index.html?v=ddef7800c7df"
         title="<?= Loc::getMessage('PROSPEKTWEB_CALC_IFRAME_TITLE') ?>">
     </iframe>
 </div>
--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=ff82d0bc7c44',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=ddef7800c7df',
     apiBase: '/bitrix/tools/prospektweb.calc/',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
