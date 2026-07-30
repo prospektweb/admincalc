25bdd3180cae1c08679faa08967e783b3e196ae0
PROSPEKT <prospektweb@gmail.com>
2026-07-30 11:07:08 +0500

    Publish nested condition editor fix

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=8c7ad32e1870',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=2bed307a7b10',
     apiBase: '/bitrix/tools/prospektweb.calc/',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
