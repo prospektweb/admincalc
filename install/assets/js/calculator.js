0140f7d945b9cf3fba3a364bbbe275514ed4cfde
PROSPEKT <prospektweb@gmail.com>
2026-07-30 14:33:23 +0500

    Publish sibling-scoped stage drag

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=4c88927bc7b8',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=52fe525f5fb8',
     apiBase: '/bitrix/tools/prospektweb.calc/',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
