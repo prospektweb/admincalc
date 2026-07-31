2bc196595c5f5264237ed9624e4922c8703a8103
PROSPEKT <prospektweb@gmail.com>
2026-07-31 20:58:12 +0500

    deploy condition branch interaction fix

--- admin/calculator.php
+++ admin/calculator.php
@@ -89,7 +89,7 @@ require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_admin_a
 <div id="calc-container">
     <iframe 
         id="calc-iframe" 
-        src="/local/apps/prospektweb.calc/index.html?v=4573ab90a4d6"
+        src="/local/apps/prospektweb.calc/index.html?v=9851ef9ff69e"
         title="<?= Loc::getMessage('PROSPEKTWEB_CALC_IFRAME_TITLE') ?>">
     </iframe>
 </div>
--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=4573ab90a4d6',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=9851ef9ff69e',
     apiBase: '/bitrix/tools/prospektweb.calc/',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
