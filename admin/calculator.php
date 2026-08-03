dec1bd85bad8024716f39660fc4866ba45d4f361
PROSPEKT <prospektweb@gmail.com>
2026-08-03 13:15:23 +0500

    Release calculator editor recovery fixes

--- admin/calculator.php
+++ admin/calculator.php
@@ -94,7 +94,7 @@ body {
 <div id="calc-container">
     <iframe 
         id="calc-iframe" 
-        src="/local/apps/prospektweb.calc/index.html?v=99ece53e103e"
+        src="/local/apps/prospektweb.calc/index.html?v=72f9720436f7"
         title="<?= Loc::getMessage('PROSPEKTWEB_CALC_IFRAME_TITLE') ?>">
     </iframe>
 </div>
--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-        appUrl: '/local/apps/prospektweb.calc/index.html?v=6ee327bb2c59',
+        appUrl: '/local/apps/prospektweb.calc/index.html?v=72f9720436f7',
     apiBase: '/bitrix/tools/prospektweb.calc/',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
