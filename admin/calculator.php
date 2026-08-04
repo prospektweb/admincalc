8ee286e336023626a74c9ddf4af70f6e20f2d0b3
PROSPEKT <prospektweb@gmail.com>
2026-08-04 19:31:41 +0500

    chore: release executed stage state carry

--- admin/calculator.php
+++ admin/calculator.php
@@ -127,7 +127,7 @@ body {
 <div id="calc-container">
     <iframe 
         id="calc-iframe" 
-        src="/local/apps/prospektweb.calc/index.html?v=c0d04de4e5d3"
+        src="/local/apps/prospektweb.calc/index.html?v=1f014a145840"
         title="<?= Loc::getMessage('PROSPEKTWEB_CALC_IFRAME_TITLE') ?>">
     </iframe>
 </div>
