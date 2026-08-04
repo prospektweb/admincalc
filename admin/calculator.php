2c320f8f17bccdd6bdbc4c146604e5a1d53be520
PROSPEKT <prospektweb@gmail.com>
2026-08-04 09:50:14 +0500

    release globals refactor apply fix

--- admin/calculator.php
+++ admin/calculator.php
@@ -127,7 +127,7 @@ body {
 <div id="calc-container">
     <iframe 
         id="calc-iframe" 
-        src="/local/apps/prospektweb.calc/index.html?v=8990efabf587"
+        src="/local/apps/prospektweb.calc/index.html?v=757e05b4786f"
         title="<?= Loc::getMessage('PROSPEKTWEB_CALC_IFRAME_TITLE') ?>">
     </iframe>
 </div>
