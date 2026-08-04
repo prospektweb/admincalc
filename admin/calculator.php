a46e6bcb2fae5b3cdebbd65a4a522e81ef6542ee
PROSPEKT <prospektweb@gmail.com>
2026-08-04 09:25:26 +0500

    Release reliable global refactor activation

--- admin/calculator.php
+++ admin/calculator.php
@@ -127,7 +127,7 @@ body {
 <div id="calc-container">
     <iframe 
         id="calc-iframe" 
-        src="/local/apps/prospektweb.calc/index.html?v=7898cd09ac3a"
+        src="/local/apps/prospektweb.calc/index.html?v=748e625213f0"
         title="<?= Loc::getMessage('PROSPEKTWEB_CALC_IFRAME_TITLE') ?>">
     </iframe>
 </div>
--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-        appUrl: '/local/apps/prospektweb.calc/index.html?v=7898cd09ac3a',
+        appUrl: '/local/apps/prospektweb.calc/index.html?v=748e625213f0',
     apiBase: '/bitrix/tools/prospektweb.calc/',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
