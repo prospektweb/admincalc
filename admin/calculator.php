d05dcdf2ff65e3e0ebba2e661aef2c8ff7e1c7a1
PROSPEKT <prospektweb@gmail.com>
2026-08-05 15:58:11 +0500

    fix(calc): publish aggregated stage warnings

--- admin/calculator.php
+++ admin/calculator.php
@@ -127,7 +127,7 @@ body {
 <div id="calc-container">
     <iframe 
         id="calc-iframe" 
-        src="/local/apps/prospektweb.calc/index.html?v=d8e242e5a8af"
+        src="/local/apps/prospektweb.calc/index.html?v=c0570dff3d8f"
         title="<?= Loc::getMessage('PROSPEKTWEB_CALC_IFRAME_TITLE') ?>">
     </iframe>
 </div>
--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-        appUrl: '/local/apps/prospektweb.calc/index.html?v=d8e242e5a8af',
+        appUrl: '/local/apps/prospektweb.calc/index.html?v=c0570dff3d8f',
     apiBase: '/bitrix/tools/prospektweb.calc/',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
