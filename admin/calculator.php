d87136867e4355ff78ec089c541ccb4679376a65
PROSPEKT <prospektweb@gmail.com>
2026-08-05 09:16:29 +0500

    fix(calc): publish stacked offer name controls

--- admin/calculator.php
+++ admin/calculator.php
@@ -127,7 +127,7 @@ body {
 <div id="calc-container">
     <iframe 
         id="calc-iframe" 
-        src="/local/apps/prospektweb.calc/index.html?v=c97bbedb5405"
+        src="/local/apps/prospektweb.calc/index.html?v=115d457cadda"
         title="<?= Loc::getMessage('PROSPEKTWEB_CALC_IFRAME_TITLE') ?>">
     </iframe>
 </div>
--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-        appUrl: '/local/apps/prospektweb.calc/index.html?v=c97bbedb5405',
+        appUrl: '/local/apps/prospektweb.calc/index.html?v=115d457cadda',
     apiBase: '/bitrix/tools/prospektweb.calc/',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
