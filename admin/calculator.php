4c42d0ea9609bb575b9d69ce971baa6d550a8740
PROSPEKT <prospektweb@gmail.com>
2026-08-06 17:35:51 +0500

    build(calc): publish preset and logic import UX

--- admin/calculator.php
+++ admin/calculator.php
@@ -127,7 +127,7 @@ body {
 <div id="calc-container">
     <iframe 
         id="calc-iframe" 
-        src="/local/apps/prospektweb.calc/index.html?v=c0570dff3d8f"
+        src="/local/apps/prospektweb.calc/index.html?v=b9ea6a16a2e5"
         title="<?= Loc::getMessage('PROSPEKTWEB_CALC_IFRAME_TITLE') ?>">
     </iframe>
 </div>
--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-        appUrl: '/local/apps/prospektweb.calc/index.html?v=c0570dff3d8f',
+        appUrl: '/local/apps/prospektweb.calc/index.html?v=b9ea6a16a2e5',
     apiBase: '/bitrix/tools/prospektweb.calc/',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
