460c377500c2cadfcb6b5d17217fb8c9cac44383
PROSPEKT <prospektweb@gmail.com>
2026-08-18 07:23:15 +0500

    fix: finish Russian preset form interface

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-        appUrl: '/local/apps/prospektweb.calc/index.html?v=b5464a006ac7',
+        appUrl: '/local/apps/prospektweb.calc/index.html?v=ddf30cc37273',
     apiBase: '/bitrix/tools/prospektweb.calc/',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
--- install/version.php
+++ install/version.php
@@ -1,5 +1,5 @@
 <?php
 $arModuleVersion = [
-    'VERSION' => '1.9.5',
-    'VERSION_DATE' => '2026-08-17 22:01:31',
+    'VERSION' => '1.9.6',
+    'VERSION_DATE' => '2026-08-18 07:21:57',
 ];
