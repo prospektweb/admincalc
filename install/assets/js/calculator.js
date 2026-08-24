b02891f1366f0f6db6e7a996b1d976f09a939c67
PROSPEKT <prospektweb@gmail.com>
2026-08-24 18:45:41 +0500

    fix(ui): publish wide field identity slider

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=49d05545130b',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=8c477b4035b9',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
--- install/version.php
+++ install/version.php
@@ -1,5 +1,5 @@
 <?php
 $arModuleVersion = [
-    'VERSION' => '1.12.4',
-    'VERSION_DATE' => '2026-08-24 12:59:56',
+    'VERSION' => '1.12.5',
+    'VERSION_DATE' => '2026-08-24 18:43:52',
 ];
