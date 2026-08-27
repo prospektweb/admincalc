64d064cf67207005ee1aa48b44864850ed44dccc
PROSPEKT <prospektweb@gmail.com>
2026-08-27 20:20:10 +0500

    build(ui): accept deterministic runtime sort

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=6b775be0e1b3',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=2120593b6daa',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
