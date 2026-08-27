5f2760063ac168f93deced94c6ae56821d0a35ae
PROSPEKT <prospektweb@gmail.com>
2026-08-27 10:25:00 +0500

    chore(ui): align calcconfig cache token

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=f37d45ca9f4c',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=ec0c960a7d81',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
