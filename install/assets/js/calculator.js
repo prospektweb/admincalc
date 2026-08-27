37afa2d6d0a66eb5ea4c25b90c77c33524d9e55d
PROSPEKT <prospektweb@gmail.com>
2026-08-27 19:05:34 +0500

    build(ui): ship exact version form isolation

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=ec48ad91859a',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=c497e81b2ce8',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
