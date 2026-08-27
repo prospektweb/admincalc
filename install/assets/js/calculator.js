4c6efef27a34fe2a68e40dcecdd2a23c1ae030c6
PROSPEKT <prospektweb@gmail.com>
2026-08-27 12:04:18 +0500

    chore(ui): ship calcconfig 6f3f60a

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=b76eac7ae6e2',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=6f3f60a6c99f',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
