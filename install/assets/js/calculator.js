c943786cfd71138eee560a06f3c99ababe19bd79
PROSPEKT <prospektweb@gmail.com>
2026-08-29 09:06:39 +0500

    build: publish logic editor workspace geometry

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=168fb8903a79',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=19d62b399385',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
