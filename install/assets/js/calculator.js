06a6e91dccbd33fc8ebc61aae39e0e035b36f0da
PROSPEKT <prospektweb@gmail.com>
2026-08-29 09:58:55 +0500

    build: publish final editor hover cascade

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=95ac47328e71',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=e8ad53c488d0',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
