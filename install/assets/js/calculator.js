98284cb94326ada6245a84a261f97101bb7f6b63
PROSPEKT <prospektweb@gmail.com>
2026-08-29 09:27:16 +0500

    build: publish sliding calculation workspace

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=19d62b399385',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=5b231fe8b767',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
