f99c02b4e04aa122f530ec89a2d6827d4db27785
PROSPEKT <prospektweb@gmail.com>
2026-08-30 19:52:53 +0500

    build: publish linked storefront form controls

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=a52739ba9e65',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=821b52059454',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
