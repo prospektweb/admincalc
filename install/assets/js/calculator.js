c1961db381e89f8917ae93a16a8e64ad8c1881ec
PROSPEKT <prospektweb@gmail.com>
2026-08-30 12:11:20 +0500

    build: publish trusted AI pilot response

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=8ce73625c4e7',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=a52739ba9e65',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
