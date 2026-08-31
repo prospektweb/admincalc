ab40dd3df305c1a68600b2a7cc49232d10940d86
PROSPEKT <prospektweb@gmail.com>
2026-08-31 16:07:49 +0500

    build: publish PDF contour authoring UI

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=7812102455a6',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=1bdc823f03aa',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
