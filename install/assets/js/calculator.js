f02cc174edebd6c4e4e43f9935486494c3a174f1
PROSPEKT <prospektweb@gmail.com>
2026-08-31 08:33:03 +0500

    build: publish focused inline field rename

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=3c9591c88419',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=87283cda3475',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
