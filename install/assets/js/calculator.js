d7917c3911a13e64921393e01797d17e8731f5bd
PROSPEKT <prospektweb@gmail.com>
2026-08-31 17:47:22 +0500

    build: publish resilient stage mappings

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=3797b9a9cfeb',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=a6f7e7b49130',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
