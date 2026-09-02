c796b833b87f9ba81b41159528c17cbcb5099e51
PROSPEKT <prospektweb@gmail.com>
2026-09-02 19:11:35 +0500

    release: publish full-width material decision tree

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=2c1f980b770d',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=7846ab8cadb6',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
--- install/version.php
+++ install/version.php
@@ -1,5 +1,5 @@
 <?php
 $arModuleVersion = [
-    'VERSION' => '1.13.51',
-    'VERSION_DATE' => '2026-09-02 18:31:56',
+    'VERSION' => '1.13.52',
+    'VERSION_DATE' => '2026-09-02 19:09:35',
 ];
