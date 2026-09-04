804a21a1d939aa898b1370fa1cfa7c0b565a1da9
PROSPEKT <prospektweb@gmail.com>
2026-09-04 15:43:50 +0500

    fix: publish accessible stage settings bundle

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=0915e384cfc5',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=755479dfc064',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
