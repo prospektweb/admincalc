864b33e6e20a7250f8e8fc69fc0c99f2c82196fe
PROSPEKT <prospektweb@gmail.com>
2026-09-04 17:12:04 +0500

    fix: publish resolved stage entity context

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=755479dfc064',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=95e753b763f2',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
