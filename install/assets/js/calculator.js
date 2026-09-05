9f3ae45feb31f16d2568bae661910f1d592e4141
PROSPEKT <prospektweb@gmail.com>
2026-09-05 08:33:15 +0500

    build: publish same-code global output fix

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=c8c8a8a1a566',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=2485babfb332',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
--- install/version.php
+++ install/version.php
@@ -1,5 +1,5 @@
 <?php
 $arModuleVersion = [
-    'VERSION' => '1.13.82',
+    'VERSION' => '1.13.83',
     'VERSION_DATE' => '2026-09-05 08:05:28',
 ];
