3afcd049169630d90c9780cb174dce5cfcef3c30
PROSPEKT <prospektweb@gmail.com>
2026-09-07 20:54:59 +0500

    Deploy registry interaction states from calcconfig 8255565

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=d114a8700467',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=8255565bc81a',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
