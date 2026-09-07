4df5385a6be459cc7f0f9fec804520d167c14ac3
PROSPEKT <prospektweb@gmail.com>
2026-09-07 23:26:26 +0500

    Release bounded native formula panel a892e6bce639

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=fb23aa6265fb',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=a892e6bce639',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
