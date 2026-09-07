8aa4d045999350dda85bb6759490c87cbaa82d84
PROSPEKT <prospektweb@gmail.com>
2026-09-07 23:37:46 +0500

    Release readable formula status badges d438674fd54d

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=639aa49774c8',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=d438674fd54d',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
