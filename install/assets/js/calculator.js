50ebf4cfd4b76d5f5b5ff810102957b0fa9c6c9e
PROSPEKT <prospektweb@gmail.com>
2026-09-08 03:45:20 +0500

    Release compact native port controls b11a2c05e403

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=2f84c291fcc1',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=b11a2c05e403',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
