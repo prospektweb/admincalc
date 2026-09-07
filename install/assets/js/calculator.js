71b012e9d3dee461af9bf61308f618d5576d0efd
PROSPEKT <prospektweb@gmail.com>
2026-09-07 21:18:24 +0500

    Deploy native folder conflict recovery UI 6cfd9a0

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=bb04afd6b509',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=6cfd9a0784f5',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
