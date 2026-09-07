e10beece02f8bab8ceaedfe6c83bfbeaf90b1a2c
PROSPEKT <prospektweb@gmail.com>
2026-09-07 21:59:19 +0500

    Ship legible native version theme surfaces

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=f62209bd65f4',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=030e0437931d',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
