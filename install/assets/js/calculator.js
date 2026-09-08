3c53237e999c27528cb5cea7743768f6ee24f098
PROSPEKT <prospektweb@gmail.com>
2026-09-08 05:45:40 +0500

    Release native branch-owned group commands and guarded actions

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=2f26479c2475',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=9a711c27cefd',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
