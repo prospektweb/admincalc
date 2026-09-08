e7e35b381ec6567f909ca1194887874086abd86a
PROSPEKT <prospektweb@gmail.com>
2026-09-08 19:16:20 +0500

    Ship accessible stage settings and semantic hover icons

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=8d1cbfdc2275',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=e803b71d1b21',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
