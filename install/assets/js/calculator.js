946e9d908ad91da3f530a1faece8658becb667e1
PROSPEKT <prospektweb@gmail.com>
2026-09-09 00:18:02 +0500

    Show native card revision conflicts in the active dialog

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=e4c272656ea9',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=ae3350a76819',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
