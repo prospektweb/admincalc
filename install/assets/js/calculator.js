2da78eaa767b61e51f94825c4a72f536d1138c3c
PROSPEKT <prospektweb@gmail.com>
2026-09-08 16:42:31 +0500

    Ship storefront undo and redo selection fix

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=8c8d32125481',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=9932100e02dd',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
