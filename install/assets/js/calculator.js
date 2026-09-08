8168cb3730f5df5292351ec85912af19038dc6cf
PROSPEKT <prospektweb@gmail.com>
2026-09-08 08:44:26 +0500

    Ship stable native context selection e152c86

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=55a721277ed0',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=e152c8687dc7',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
