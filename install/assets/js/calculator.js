dcdc38d40a7a1b29e8815fda94a04a7cec3373e9
PROSPEKT <prospektweb@gmail.com>
2026-09-08 05:53:06 +0500

    Release corrected empty branch insertion anchor and metadata surface

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=9a711c27cefd',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=cfbe12e06918',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
