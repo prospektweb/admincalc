0796b62e7b9656c6c1624128e21d31274e4d273c
PROSPEKT <prospektweb@gmail.com>
2026-09-08 18:15:35 +0500

    Ship native pricing button theme correction

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=f90f9c501a1d',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=00c13321c5a5',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
