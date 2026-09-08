b4c9fa8d602c4a80ca4136b0f97ef79b4a2ff9e6
PROSPEKT <prospektweb@gmail.com>
2026-09-08 07:28:54 +0500

    Ship visible binding expansion state ccf7d5e

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=5227f9ec5283',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=ccf7d5e803ed',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
