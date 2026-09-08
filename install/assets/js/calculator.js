6b4da3ed87ad30068ee2f5ae2f34d89611986dc5
PROSPEKT <prospektweb@gmail.com>
2026-09-08 08:32:39 +0500

    Ship original context shell and native publication flow b9f2dd0

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=67d8286a93b9',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=b9f2dd05890b',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
