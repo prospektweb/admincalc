fd3f0ca0f74144f82ff1d8bb2b1d7f6db2385e76
PROSPEKT <prospektweb@gmail.com>
2026-09-08 21:47:51 +0500

    Ship native automatic rule table and compact dialog fixes

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=8a6ffb343453',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=606b95a9932a',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
