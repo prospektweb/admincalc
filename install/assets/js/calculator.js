17e2863414b0271bf7d738b22e4620616189b680
PROSPEKT <prospektweb@gmail.com>
2026-09-08 19:06:39 +0500

    Ship stable stage settings interactions and archived read-only view

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=905120eef816',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=8d1cbfdc2275',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
