c6aaa064610edc272bc144fb1e6bb49d9af11345
PROSPEKT <prospektweb@gmail.com>
2026-09-08 09:12:59 +0500

    Ship mapping state and paired draft comparison fixes df3bd33

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=4bb29dece59e',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=df3bd33cef24',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
