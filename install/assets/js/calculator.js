0e852cdb31cc376afa68a5523f409810ebd1cbbb
PROSPEKT <prospektweb@gmail.com>
2026-09-08 06:14:10 +0500

    Release original condition branch editor with native document controller

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=cfbe12e06918',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=c8f211e250cf',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
