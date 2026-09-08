c4066d5f57e81089310db97f0c43743d614ff401
PROSPEKT <prospektweb@gmail.com>
2026-09-08 13:42:38 +0500

    Ship native report grouping and manual report identity UI

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=bda675e40eba',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=55444b94db8f',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
