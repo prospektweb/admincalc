c4734ef52d372a5a9c8f9b9149b1a0d5285333b0
PROSPEKT <prospektweb@gmail.com>
2026-09-09 04:27:27 +0500

    Ship restored direct resource cards and compact native stage folder

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=af05b4f6d9c3',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=a67536114a73',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
