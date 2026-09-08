d6d92934029f945a900101de03ee5806b72f019c
PROSPEKT <prospektweb@gmail.com>
2026-09-08 14:02:15 +0500

    Ship native manual report editing and reset lifecycle

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=a0d337a73161',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=8ad2e2b02b18',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
