28c870c5ad0cf14d2b481b9c2200dd4fd48ee15f
PROSPEKT <prospektweb@gmail.com>
2026-09-09 04:49:49 +0500

    Ship native scheme creation in the stage selector

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=a97141f33f6c',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=717bb630c01d',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
