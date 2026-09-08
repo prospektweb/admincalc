b9a1b5d879b685ffc4fd69c8ec3f1ec2e2c427e1
PROSPEKT <prospektweb@gmail.com>
2026-09-08 18:23:07 +0500

    Ship pricing conflict feedback and destructive confirmation styling

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=00c13321c5a5',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=f98fd00d5613',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
