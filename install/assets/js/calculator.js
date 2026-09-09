ad91e396aedbe86d324ce9a0b59de23e1eea269b
PROSPEKT <prospektweb@gmail.com>
2026-09-09 08:19:57 +0500

    Preserve visible field rows in stage settings layout

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=5beae999ee7d',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=bc08f9e114f2',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
