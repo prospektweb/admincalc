29d2a3517541b02569ddb3935dfdbdda1480157d
PROSPEKT <prospektweb@gmail.com>
2026-09-09 05:52:27 +0500

    Ship native formula tests and shared diagnostic engine

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=937f455f2f18',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=4d66d49830e3',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
