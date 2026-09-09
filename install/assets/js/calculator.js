6a49d07e9bcfa6141d7cd8e741e9cea29ae3bbdc
PROSPEKT <prospektweb@gmail.com>
2026-09-09 22:51:45 +0500

    build(calcconfig): restore logic editor layout and resource context

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=a60f09e5d1ff',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=bdfd85d8452c',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
