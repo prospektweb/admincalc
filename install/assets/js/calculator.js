8de4188a48ab0f492aab3d91df49c15d0233c796
PROSPEKT <prospektweb@gmail.com>
2026-09-09 07:41:15 +0500

    Ship native custom field definition cards

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=5654cb701fcb',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=e7f8ceada55b',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
