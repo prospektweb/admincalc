044358244f2047dd74ffe6cb55df42bc1da73935
PROSPEKT <prospektweb@gmail.com>
2026-09-09 08:42:28 +0500

    Ship accessible stage settings without retired parameter description

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=528b9f34808c',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=5e7670ae64b2',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
