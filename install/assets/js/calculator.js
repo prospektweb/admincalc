5f1c8637c86f8bef027debba64ab7e170b75231c
PROSPEKT <prospektweb@gmail.com>
2026-09-09 06:50:47 +0500

    Ship archived product navigation guard fix

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=aa6d13ae9c69',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=02be88f54301',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
