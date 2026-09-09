de27cc6583505d42797a9978bb72a85ce30fc052
PROSPEKT <prospektweb@gmail.com>
2026-09-09 08:34:57 +0500

    Ship native literal stage inputs and retire additional field catalog UI

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=bc08f9e114f2',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=528b9f34808c',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
