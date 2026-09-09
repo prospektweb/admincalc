0b9666cfb3afa5a258dfe34bad62a68837fc5398
PROSPEKT <prospektweb@gmail.com>
2026-09-09 14:10:33 +0500

    Ship original stage action layout and neutral selection chrome c52fcbe5b3af

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=2c5bfeaa7168',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=c52fcbe5b3af',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
