9556da0884519839a9df3982f37dc98e6f5ecd9a
PROSPEKT <prospektweb@gmail.com>
2026-09-09 06:11:51 +0500

    Ship native logic snapshot import and export

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=4d66d49830e3',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=3b4d276ee1a0',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
