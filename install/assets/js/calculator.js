681461cee3262e07fa5500afe73013763322623e
PROSPEKT <prospektweb@gmail.com>
2026-09-09 22:07:36 +0500

    Ship control center UX and theme refinement bundle

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=332c7f2d5000',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=dc690ca1b886',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
