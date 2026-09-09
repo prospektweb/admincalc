8c7555da3755e001960fafe4761862f29eb95d73
PROSPEKT <prospektweb@gmail.com>
2026-09-09 06:45:12 +0500

    Ship accurate native product assignment status

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=d895219515fa',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=aa6d13ae9c69',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
