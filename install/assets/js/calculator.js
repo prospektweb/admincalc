30090c29f4a3a8530d9a038c93242e286fd5f5c8
PROSPEKT <prospektweb@gmail.com>
2026-09-09 14:04:01 +0500

    Ship restored native calculator workbench UI 2c5bfeaa7168

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=b7cb3bf7bc35',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=2c5bfeaa7168',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
