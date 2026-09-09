98e4f4d2d18c5d27469058bc989eb953f004c07e
PROSPEKT <prospektweb@gmail.com>
2026-09-09 07:59:33 +0500

    Ship version-owned field directory controls

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=e7f8ceada55b',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=5e6d6ed4aa1b',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
