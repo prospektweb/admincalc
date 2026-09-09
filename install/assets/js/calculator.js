e489e9341e4c928859e8c2ddad3a29b50b92f402
PROSPEKT <prospektweb@gmail.com>
2026-09-09 05:20:50 +0500

    Ship real native folder paths in scheme cards

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=f116a7a2de03',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=a08d0fcc87df',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
