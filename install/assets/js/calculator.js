ec3511a92f191605cafd8e94df532a7c125fdf57
PROSPEKT <prospektweb@gmail.com>
2026-09-12 21:16:07 +0500

    build: refresh packet recovery interface

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=36839ce2e5bf',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=0c7228821fc7',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
