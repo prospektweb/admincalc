9472fa0617ec40b4c2c15f27bc47b3725a0432b1
PROSPEKT <prospektweb@gmail.com>
2026-09-12 13:15:04 +0500

    Publish snapshot light theme control fix

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=d9c892b1a452',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=b565cd281746',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
