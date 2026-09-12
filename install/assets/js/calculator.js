01cadba534789285519486692077c8e18a655b74
PROSPEKT <prospektweb@gmail.com>
2026-09-12 19:19:38 +0500

    build: publish compact saved preparation inputs

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=b42180423107',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=762432a61579',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
