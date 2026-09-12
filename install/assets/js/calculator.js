01ee23f2ec799e35ed5c066455092fd31ce21502
PROSPEKT <prospektweb@gmail.com>
2026-09-12 18:46:33 +0500

    build: publish compact group menus and resilient rename

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=38c97420880a',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=f6b6ac4fea65',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
