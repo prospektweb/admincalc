c0614c6a1a8610d7da65d5d992890008b6b482c3
PROSPEKT <prospektweb@gmail.com>
2026-09-12 18:28:39 +0500

    build: publish bidirectional group reordering

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=bef6f3a26a82',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=38c97420880a',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
