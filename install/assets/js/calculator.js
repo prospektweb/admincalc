7186cf33d34a96a8949707f663a1aa42032c5829
PROSPEKT <prospektweb@gmail.com>
2026-09-13 07:30:39 +0500

    build: publish preparation split viewer and full-height list

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=73cf4aa47851',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=703cd0a81a43',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
