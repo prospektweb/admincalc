c7cc1d97f748e57f0775a8c9c63b80605f92364f
PROSPEKT <prospektweb@gmail.com>
2026-09-13 10:24:42 +0500

    build: publish unclipped property option viewers

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=c4f4993342d0',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=e5d05691ab5e',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
