1b3ca900610ffe81a153b57e82af37ca2d41c91a
PROSPEKT <prospektweb@gmail.com>
2026-09-14 07:52:58 +0500

    Deploy storefront scenario chip editor 865074c

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=4ba32f000fb7',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=865074c32bde',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
