10ade7dc5d9916350a16cf9c13cfd7c13c1a65b9
PROSPEKT <prospektweb@gmail.com>
2026-09-15 19:46:29 +0500

    fix(layouts): deliver direct Yandex credential links

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=fa95c5b043eb',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=257d7bab7e82',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
