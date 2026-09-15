5d445361ea9235130a5cb81f0022971074d8d655
PROSPEKT <prospektweb@gmail.com>
2026-09-15 13:19:33 +0500

    fix(control-center): deliver compact policy editor

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=ce5c3d8b93a5',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=51f3c1251831',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
