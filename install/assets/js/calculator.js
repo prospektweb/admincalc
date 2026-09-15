55e40247a8502b20fc557d66a1e1bb3fffb46ffa
PROSPEKT <prospektweb@gmail.com>
2026-09-15 17:56:16 +0500

    fix(orderterms): deliver pinned storefront policy context

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=a146f6146730',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=e5947177c4da',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
