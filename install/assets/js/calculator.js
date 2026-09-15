73164bc0d09824d1a3ebe4052e8e4ecea2e0e6e5
PROSPEKT <prospektweb@gmail.com>
2026-09-15 20:18:46 +0500

    fix(control-center): deliver themed settings selectors

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=50fdecafc22f',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=15cc7577cafc',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
