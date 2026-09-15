3503112df5c145ad883b6755c9ac7cbc9618a98d
PROSPEKT <prospektweb@gmail.com>
2026-09-15 18:57:11 +0500

    build(layouts): deliver OAuth setup links

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=66189ec5b225',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=c02fd59e8cde',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
