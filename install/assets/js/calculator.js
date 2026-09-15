526a190316eaf709427c64513f0dd9f708f91880
PROSPEKT <prospektweb@gmail.com>
2026-09-15 13:29:22 +0500

    fix(control-center): deliver hidden-field auxiliary control guard

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=51f3c1251831',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=a146f6146730',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
