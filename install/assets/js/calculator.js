2482c9a3062d9992f426240371e5d2f92b77396f
PROSPEKT <prospektweb@gmail.com>
2026-09-15 10:22:53 +0500

    fix(control-center): deliver calendar state and global selector refinements

--- install/assets/js/calculator.js
+++ install/assets/js/calculator.js
@@ -8,7 +8,7 @@ console.log('[BitrixBridge] calculator.js loaded, init integration...');
 
 var ProspekwebCalc = {
     // Пути
-    appUrl: '/local/apps/prospektweb.calc/index.html?v=380e83805a59',
+    appUrl: '/local/apps/prospektweb.calc/index.html?v=d96aaa43594a',
     cssPath: '/local/css/prospektweb.calc/calculator.css',
 
     loadCss: function(href) {
